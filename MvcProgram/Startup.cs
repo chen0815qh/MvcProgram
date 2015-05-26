05deec6bd874d77edc67d38a9409b188d23ed9ae
chen0815qh <990603465@qq.com>
2015-05-26 23:24:50 +0800

    第一次代码提交

---
+++ MvcProgram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcProgram.Startup))]
+namespace MvcProgram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
