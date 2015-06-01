4a280ddf3b20c623a1c5895f9854f17d244fa104
chen0815qh <990603465@qq.com>
2015-06-01 14:48:34 +0800

    最后一次提交

--- MODEL/ExtentionModel/Members.cs
+++ MODEL/ExtentionModel/Members.cs
@@ -11,41 +11,14 @@ namespace MODEL
     [Serializable]
     public partial class Members
     {
-        [Display(Name="确认密码")]
-        [Required(ErrorMessage="确认密码不能为空")]
-        [Compare("Password",  ErrorMessage = "密码和确认密码必须一致")]
+        [Display(Name = "确认密码")]
+        [Required(ErrorMessage = "确认密码不能为空")]
+        [Compare("Password", ErrorMessage = "密码和确认密码必须一致")]
         //当提交表单时，会将该字段的值和Password进行比较，如果不相等，则验证不通过。
         public string ConfirmPassword
         {
             get;
             set;
         }
-
-        //public int MemberId { get; set; }
-        //[Display(Name = "会员账号")]
-        //[Required(ErrorMessage = "请输入会员账号")]//表示必须要输入。
-        //public string MemberAccount { get; set; }
-        //[Display(Name = "头像")]
-
-        //public string Portrait { get; set; }
-        //[Display(Name = "昵称")]
-        //public string NickName { get; set; }
-        //[Display(Name = "性别")]
-        //public string Gender { get; set; }
-        //[Display(Name = "年龄")]
-        //[Required(ErrorMessage = "请输入年龄")]
-        //[RegularExpression(@"^[1-9](\d{1,2})?$", ErrorMessage = "请输入正确的年龄")]
-        //[Range(1, 100, ErrorMessage = "年龄太大或太小")]//该步骤实际上可以在一个正则表达式中完成。
-        //public Nullable<int> Age { get; set; }
-        //[RegularExpression(@"^[1-9][0-9]{4,}$", ErrorMessage = "请输入正确的QQ号")]
-        //public string QQ { get; set; }
-        //[RegularExpression(@"^w+([-+.]w+)*@w+([-.]w+)*.w+([-.]w+)* $", ErrorMessage = "请输入正确的Email")]
-        //public string Email { get; set; }
-        //public Nullable<System.DateTime> RegisterTime { get; set; }
-        //public Nullable<System.DateTime> LastLogin { get; set; }
-        //public string LastLoginIP { get; set; }
-        //[Required(ErrorMessage = "密码不能为空")]
-        //[Display(Name = "密码")]
-        //public string Password { get; set; }
     }
 }
